3397350158aaf8a38b8a7781e9dcebb56c83f0d6
MSI\amith <amithrc07@gmail.com>
2019-01-27 14:48:20 -0500

    Added Util

--- src/main/java/projectRunner.java
+++ src/main/java/projectRunner.java
@@ -1,20 +1,23 @@
 package main.java;
 
+import main.java.util.Util;
 import main.java.util.constants;
 import main.java.indexer.indexBuilder;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class projectRunner
 {
     private static void  usage() {
         System.out.println("args[0] --> Paragraph corpus");
+        System.out.println("args[1] --> Outline CBOR");
         System.exit(-1);
     }
 
     public static void main(String[] args) {
         String dest;
-        if (args.length < 1) {
+        if (args.length < 2) {
             usage();
         } else
             {
@@ -29,7 +32,15 @@ public class projectRunner
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+
+                Map<String,String> p = Util.readOutline(args[1]);
+                for(Map.Entry<String,String> obj:p.entrySet())
+                {
+                    System.out.println(obj.getKey()+"------------->"+obj.getValue());
+                }
+
             }
+
     }
 
 }
---
+++ src/main/java/util/Util.java
@@ -0,0 +1,53 @@
+package main.java.util;
+import edu.unh.cs.treccar_v2.read_data.DeserializeData;
+import edu.unh.cs.treccar_v2.Data;
+import java.io.*;
+import java.util.*;
+
+public class Util
+{
+    static public Map<String, String> readOutline(String filename) {
+        Map<String, String> data = new LinkedHashMap<String, String>();
+
+        FileInputStream qrelStream = null;
+        try {
+            qrelStream = new FileInputStream(new File(filename));
+        } catch (FileNotFoundException fnf) {
+            System.out.println(fnf.getMessage());
+        }
+        for (Data.Page page : DeserializeData.iterableAnnotations(qrelStream))
+        {
+            data.put(page.getPageId(), page.getPageName());
+        }
+        return data;
+    }
+
+    static public Map<String, String> readOutlineSectionPath(String filename) {
+        Map<String, String> data = new LinkedHashMap<String, String>();
+
+        FileInputStream qrelStream = null;
+        try {
+            qrelStream = new FileInputStream(new File(filename));
+        } catch (FileNotFoundException fnf) {
+            System.out.println(fnf.getMessage());
+
+        }
+        for (Data.Page page : DeserializeData.iterableAnnotations(qrelStream))
+        {
+            StringBuilder queryBuilder = new StringBuilder();
+            queryBuilder.append(page.getPageName());
+
+            for (List<Data.Section> sectionPath : page.flatSectionPaths())
+            {
+                for(Data.Section section:sectionPath)
+                {
+                    queryBuilder.append(" ");
+                    String  result = section.getHeading().replaceAll("[^\\w\\s]","");
+                    queryBuilder.append(result);
+                }
+            }
+            data.put(page.getPageId(), queryBuilder.toString());
+        }
+        return data;
+    }
+}
