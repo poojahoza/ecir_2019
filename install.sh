c2de88d50ed584003a50cfc52aa1c5794707f496
amithrc <amithrc07@gmail.com>
2019-02-21 05:46:03 -0500

    Adding install script

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #@Author- Amith RC
 
+index_path=/home/team1/indexed_file/
+qrels_path=/home/team1/benchmarkY1Test-manual-qrels/manual.benchmarkY1test.cbor.hierarchical.qrels
+embeddings_path=/home/team1/glove_word_embeddings/glove.6B.50d.txt
+outlines_path=/home/team1/query_data/benchmarkY1-test/test.pages.cbor-outlines.cbor
+entities_path=/home/team1/entity.lucene/
+ham_train=/home/team1/hamTrain
+spam_train=/home/team1/spamTrain
+hamspam_test=/home/team1/hamSpamTest
 
 function usage()
 {
@@ -28,13 +36,20 @@ function change_path_target()
 	echo "**********************************************************"
 	pwdCurrent=$(pwd)
 	pwdCurrent=$pwdCurrent/target/cs953-team1-1.0-SNAPSHOT-jar-with-dependencies.jar
-	sudo java -jar $pwdCurrent search $1 $2 --section
+        java -jar $pwdCurrent search -i $index_path -q $outlines_path --dice-sim --cosine-sim --jaccard-sim --jaro-sim 
+        java -jar $pwdCurrent search -i $index_path -q $outlines_path --rerank -we $embeddings_path -dim=50 -k=300 --bias-fact=20
+        java -jar $pwdCurrent search -i $index_path -q $outlines_path --rerank-idf -we $embeddings_path -dim=50 -k=300 --bias-fact=20
+        java -jar $pwdCurrent search -i $index_path -q $outlines_path --rerank-df -we $embeddings_path -dim=50 -k=300 --bias-fact=20
+        java -jar $pwdCurrent search --entity-degree -i $index_path -q $outlines_path --entity-index $entities_path 
+        java -jar $pwdCurrent search --entity-expand -i $index_path -q $outlines_path --entity-index $entities_path 
+        #java -jar $pwdCurrent filter -i $index_path $spam_train $ham_train $hamspam_test
+        java -jar $pwdCurrent search --entity-sim -i $index_path -q $outlines_path --entity-index $entities_path -we $embeddings_path -dim=50 
 }
 
 if [ $# -eq 0 ]
 then 
 	call_maven 
-	change_path_target /home/team1/indexed_file /home/team1/query_data/benchmarkY1/benchmarkY1-train/train.pages.cbor-outlines.cbor
+	change_path_target index_path outlines_path 
 elif [ $# -ne 2 ]
 then 
 	usage
