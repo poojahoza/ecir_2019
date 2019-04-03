1b296d2070452da0febc62af5ab9b787c085ac07
regaleaglebeagle <rachelcates2016@gmail.com>
2019-04-03 10:51:57 -0400

    this is the right file now.

--- svm.py
+++ svm.py
@@ -3,14 +3,15 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
-data = pd.read_csv("/home/rachel/grad_courses/data_science/datacsv.csv", error_bad_lines=False) # Check if pandas converts data to numerical values by default.
-data.dropna()
-print(data.dtypes == np.complex_)
-X = data.drop('Class', axis=1)
-y = data['Class']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+train = pd.read_csv("/home/rachel/grad_courses/data_science/train_data.csv") # Check if pandas converts data to numerical values by default.
+test = pd.read_csv("/home/rachel/grad_courses/data_science/test_data.csv")
+
+X_train = train.drop('Class', axis=1)
+y_train = train['Class']
+
 svclassifier = SVC(kernel='linear')
 svclassifier.fit(X_train, y_train)
-'''y_pred = svclassifier.predict(X_test)
-print(confusion_matrix(y_test,y_pred))
-print(classification_report(y_test,y_pred))'''
+y_pred = svclassifier.predict(test)
+for prediction in y_pred:
+    print(prediction)
+
