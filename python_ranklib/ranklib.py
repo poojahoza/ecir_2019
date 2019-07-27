5c394e65ad60891fb865bf21f20d52a01d41b6cb
poojahoza <oza.pooja.h@gmail.com>
2019-07-27 16:01:12 -0400

    ranklib change

--- python_ranklib/ranklib.py
+++ python_ranklib/ranklib.py
@@ -224,8 +224,7 @@ def process_qrel_file(qrel_file_path):
     with open(qrel_file_path, 'r') as qrel_file:
         for line in qrel_file:
             line_split = line.strip('\n').split()
-            if int(line_split[2]) == 1:
-                qrel_dict[line_split[0]].append(line_split[2])
+            qrel_dict[line_split[0]].append(line_split[2])
     # print(qrel_dict)
     return qrel_dict
 
